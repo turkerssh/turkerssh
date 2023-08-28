3d51d1aa1aa778dc762e139341e151012e8e5397
turkwr <63150613+turkwr@users.noreply.github.com>
2023-08-28 09:57:18 +0300

    update

--- src/config.js
+++ src/config.js
@@ -1,11 +1,3 @@
-
-export const activity = {
- open: "<!--START_SECTION:activity-->",
- close: "<!--END_SECTION:activity-->",
- gitUsername: "turkwr",
- maxLines: 10,
-};
-
 export const wakatime = {
  open: "<!--START_SECTION:wakatime-->",
  close: "<!--END_SECTION:wakatime-->",
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import { activity, wakatime } from "./config.js";
+import { wakatime } from "./config.js";
 import { fetchActivities } from "./updaters/fetchActivities.js";
 import { fetchCodingStats } from "./updaters/fetchCodingStats.js";
 
 const start = new Date().getTime();
 const readme = await Deno.readTextFile("./README.md", "utf8");
 
-const [activityList, statsList] = await Promise.all([fetchActivities(activity.gitUsername), fetchCodingStats(wakatime.apiKey, activity.gitUsername)]);
-
-const readmeActivity = `${readmePosts.substring(0, readmePosts.indexOf(activity.open) + activity.open.length)}\n${activityList.join("<br>")}\n${readmePosts.substring(readmePosts.indexOf(activity.close))}`;
+const [activityList, statsList] = await Promise.all([fetchCodingStats(wakatime.apiKey, "turkwr")]);
 const wakatimeActivity = `${readmeActivity.substring(0, readmeActivity.indexOf(wakatime.open) + wakatime.open.length)}\n${statsList}\n${readmeActivity.substring(readmeActivity.indexOf(wakatime.close))}`;
 await Deno.writeTextFile("./README.md", wakatimeActivity.trim());
 
